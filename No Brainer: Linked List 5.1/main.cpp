bd8dc7160568a3ad242c5e6e36c72f3e66f6f6ee
Coleton Watt <electricninja102@gmail.com>
2022-02-07 12:37:49 -0700

    Initial Commit

---
+++ No Brainer: Linked List 5.1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  No Brainer: Linked List 5.1
+//
+//  Created by Coleton Watt on 2/7/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
